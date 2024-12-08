d6b36f3cf68907e4ede3bf5b61d5b54fcbce00c9
JoaoFernandottk <joao.fernando1133@gmail.com>
2024-12-08 19:42:48 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './componentes/Header/Header.jsx';
+import './GlobalStyle/GlobalStyle.scss';
+
+export default function App() { ;
+  return (
+    <section>
+      <Header />
+    </section>
+  );
+}
